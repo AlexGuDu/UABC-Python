48ec3069ad15a78f421b2f9e9bf50e1aa6a744f9
Alex Gu Du <alex.gmole@gmail.com>
2018-12-07 01:57:05 -0800

    quick visual fix c:

--- Parcial 3/complaincity/complain_o_citizen_2000/models.py
+++ Parcial 3/complaincity/complain_o_citizen_2000/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class User(models.Model):
@@ -10,6 +11,9 @@ class BigComplaint(models.Model):
     body = models.TextField(default='empty')
     type_complaint = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
+    date = models.DateTimeField(default=datetime.now, blank=True)
 
 class MiniComplaint(models.Model):
     body = models.TextField(default='empty')
--- Parcial 3/complaincity/complain_o_citizen_2000/views.py
+++ Parcial 3/complaincity/complain_o_citizen_2000/views.py
@@ -4,10 +4,25 @@ from .models import User, BigComplaint, MiniComplaint
 
 # Create your views here.
 def index(request):
-    return render(request, 'complain_o_citizen_2000/index.html')
+    all_counts = []
+    for i in range(6):
+        all_counts.append(BigComplaint.objects.filter(type_complaint=i+1).count())
+    context = {
+        'all_counts': all_counts,
+    }
+
+    return render(request, 'complain_o_citizen_2000/index.html', context)
 
 def type_complaint(request,id):
     complaints = BigComplaint.objects.filter(type_complaint=id)
+    users = User.objects.all()
+    for user in users:
+        for complaint in complaints:
+            if len(complaint.body) > 60:
+                complaint.body = complaint.body[:50] + "..."
+            if user.id == complaint.user_id:
+                complaint.by_username = user.username
+
     context = {
         'complaints': complaints
     }
